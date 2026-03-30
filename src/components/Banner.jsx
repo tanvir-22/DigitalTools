3aced82b70f944eff2b9a7fcf91ffc98776bce3c
tanvir-22 <tanvir.mazumder01@gmail.com>
2026-03-30 20:00:31 +0600

    path issue solved

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Play } from 'lucide-react';
-
+import banner from '../assets/banner.png'
+import dot from '../assets/Group 5.png'
 function Banner() {
   return (
     <div className="hero bg-base-200 min-h-screen ">
   <div className="hero-content flex-col lg:flex-row-reverse">
     <img
-      src='../src/assets/banner.png'
+      src={banner}
       className="max-w-sm rounded-lg shadow-2xl w-[86%]"
     />
     <div className='md:w-1/2 text-center md:text-left'>
     <div className='bg-[#E1E7FF] md:p-2 gap-1  rounded-full w-[60%]  mx-auto md:mx-0 flex items-center md:gap-3'>
-      <img   src="../src/assets/Group 5.png" alt="" />
+      <img   src={dot} alt="" />
       <p className='font-semibold bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent '>New: AI-Powered Tools Available</p>
     </div>
       <h1 className="text-xl md:text-6xl  font-extrabold md:leading-21">Supercharge Your Digital Workflow</h1>
--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import user from '../assets/user.png'
+import pack from '../assets/package.png'
+import rocket from '../assets/rocket.png'
 
 function Start() {
   return (
@@ -12,21 +14,21 @@ function Start() {
     <div className='mt-8 grid md:grid-cols-3 grid-cols-1 gap-3 place-items-center'>
    <div className='w-90 px-10 py-15 shadow-2xl flex flex-col space-y-2 justify-center items-center'>
          
-             <img className=' bg-[#f4e8ff] rounded-md flex items-center justify-center p-4' src="../src/assets/user.png" alt="" />
+             <img className=' bg-[#f4e8ff] rounded-md flex items-center justify-center p-4' src={user} alt="" />
          
             <h2 className='text-xl font-extrabold'>Create Account</h2>
             <p className='text-center text-[#627382]'>Sign up for free in seconds. No credit card required to get started.</p>
         </div>
          <div className='w-90 px-10 py-15 shadow-2xl flex flex-col space-y-2 justify-center items-center'>
          
-             <img className=' bg-[#f4e8ff] rounded-md flex items-center justify-center p-4' src="../src/assets/package.png" alt="" />
+             <img className=' bg-[#f4e8ff] rounded-md flex items-center justify-center p-4' src={pack} alt="" />
          
             <h2 className='text-xl font-extrabold'>Choose Products</h2>
             <p className='text-center text-[#627382]'>Browse our catalog and select the tools that fit your needs.</p>
         </div>
          <div className='w-90 px-10 py-15 shadow-2xl flex flex-col space-y-2 justify-center items-center'>
          
-             <img className=' bg-[#f4e8ff] rounded-md flex items-center justify-center p-4' src="../src/assets/rocket.png" alt="" />
+             <img className=' bg-[#f4e8ff] rounded-md flex items-center justify-center p-4' src={rocket} alt="" />
          
             <h2 className='text-xl font-extrabold'>Start Creating</h2>
             <p className='text-center text-[#627382]'>Download and start using your premium tools immediately.</p>
